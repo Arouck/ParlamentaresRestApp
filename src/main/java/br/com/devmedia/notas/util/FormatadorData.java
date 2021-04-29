fefb243ef2a0787c795b22ad44cfa2117c1f972d
Arouck <ppvitorr@gmail.com>
2021-04-29 18:24:54 -0300

    Create FormatadorData utility class

---
+++ src/main/java/br/com/devmedia/notas/util/FormatadorData.java
@@ -0,0 +1,36 @@
+package br.com.devmedia.notas.util;
+
+import br.com.devmedia.notas.model.Despesa;
+
+import java.text.DateFormatSymbols;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+public class FormatadorData {
+
+    public String formatarData(String data) {
+        String [] dataSplit = data.split("-");
+        Collections.reverse(Arrays.asList(dataSplit));
+        return String.join("/", dataSplit);
+    }
+
+    public void formatarData(List<Despesa> despesasNoMes) {
+        for (Despesa despesa : despesasNoMes) {
+            if(despesa.getDataDocumento() != null) {
+                String [] data = despesa.getDataDocumento().split("-");
+                Collections.reverse(Arrays.asList(data));
+                despesa.setDataDocumento(String.join("/", data));
+            }
+        }
+    }
+
+    public void formatarMes(List<Despesa> despesasNoMes) {
+        DateFormatSymbols dateFormatSymbols = new DateFormatSymbols();
+        for(Despesa despesa : despesasNoMes) {
+            despesa.setMes(
+                    dateFormatSymbols.getMonths()[Integer.parseInt(despesa.getMes()) - 1]
+            );
+        }
+    }
+}
