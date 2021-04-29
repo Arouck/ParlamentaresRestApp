794bf1dad1443ed9a4a5f745e53bf1301971a3c6
Arouck <ppvitorr@gmail.com>
2021-04-29 18:17:43 -0300

    Create UltimoStatus model

---
+++ src/main/java/br/com/devmedia/notas/model/UltimoStatus.java
@@ -0,0 +1,33 @@
+package br.com.devmedia.notas.model;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class UltimoStatus {
+    private String siglaPartido;
+    private String urlFoto;
+
+    public String getSiglaPartido() {
+        return siglaPartido;
+    }
+
+    public void setSiglaPartido(String siglaPartido) {
+        this.siglaPartido = siglaPartido;
+    }
+
+    public String getUrlFoto() {
+        return urlFoto;
+    }
+
+    public void setUrlFoto(String urlFoto) {
+        this.urlFoto = urlFoto;
+    }
+
+    @Override
+    public String toString() {
+        return "UltimoStatus{" +
+                "siglaPartido='" + siglaPartido + '\'' +
+                ", urlFoto='" + urlFoto + '\'' +
+                '}';
+    }
+}
