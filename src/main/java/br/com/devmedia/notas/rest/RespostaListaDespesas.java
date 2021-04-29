83bf8c456eceb7f55e08bc951feec16e386f4276
Arouck <ppvitorr@gmail.com>
2021-04-29 18:20:19 -0300

    Create RespostaListaDespesas response manager

---
+++ src/main/java/br/com/devmedia/notas/rest/RespostaListaDespesas.java
@@ -0,0 +1,8 @@
+package br.com.devmedia.notas.rest;
+
+import br.com.devmedia.notas.model.Despesa;
+
+import java.util.List;
+
+public class RespostaListaDespesas extends RespostaAbstract<List<Despesa>> {
+}
